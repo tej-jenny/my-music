e7ec26594a5755ed2ffee0df70a4c3a68e4c6bfc
Jenny Dinh <jennythi.d@gmail.com>
2020-08-10 18:17:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Styles from './Styles/styles.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div>HELLO WORLD</div>
+    );
+  }
+}
+
+export default App;
