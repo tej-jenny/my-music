4778b053b5f792b6edf5c607ee50d2c08052eb52
Jenny Dinh <jennythi.d@gmail.com>
2020-08-10 22:46:09 -0400

    adding style and landing page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import MainController from './Components/Controller/MainController';
 import SideNav from './Components/SideNav/SideNav';
 import Main from './Components/MainProject/Main';
+import 'font-awesome/css/font-awesome.min.css';
+import LandingPage from './Components/Layout/LandingPage';
 
 const App = () => {
   return (
     <div>
+      <LandingPage />
       <div>
         <SideNav />
         <Main />
---
+++ src/Components/Layout/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <div className="heroContent">
+                <h1>Welcome to My-Music</h1>
+                <p>Organize, view and create Spotify playlists easily!</p>
+                <p>Login to Spotify to try it out!</p>
+                <button className="login"><i className="fa fa-spotify" aria-hidden="true"></i>Log in with Spotify</button>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
