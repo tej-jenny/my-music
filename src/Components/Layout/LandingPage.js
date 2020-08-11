3b40dc78b380d3ebae6edb7e106c74e8b1d53bef
Jenny Dinh <jennythi.d@gmail.com>
2020-08-10 22:56:59 -0400

    add button style

--- src/Components/Layout/LandingPage.js
+++ src/Components/Layout/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
                 <h1>Welcome to My-Music</h1>
                 <p>Organize, view and create Spotify playlists easily!</p>
                 <p>Login to Spotify to try it out!</p>
-                <button className="login"><i className="fa fa-spotify" aria-hidden="true"></i>Log in with Spotify</button>
+                <button className="btn login"><i className="fa fa-spotify" aria-hidden="true"></i>Log in with Spotify</button>
             </div>
         </div>
     )
