7f9471e38f30be6a3296a1bf545f89e6ddb23cd6
Tej Lehal <tejlehal13@gmail.com>
2020-08-11 20:59:10 -0400

    Added Carousel.js to the components

--- src/Components/Authenication/Auth.js
+++ src/Components/Authenication/Auth.js
@@ -1,12 +1,5 @@
 import React from "react";
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
-import image1 from "./../../Assets/image1.jpg";
-import image2 from "./../../Assets/image2.jpg";
-import image3 from "./../../Assets/image3.jpg";
-import image4 from "./../../Assets/image4.jpg";
-import image5 from "./../../Assets/image5.jpg";
-import image6 from "./../../Assets/image6.jpg";
+import Carousel from './Carousel'
 
 const Auth = ({ loggedIn }) => {
   return (
@@ -19,14 +12,7 @@ const Auth = ({ loggedIn }) => {
           <i className="fa fa-spotify" aria-hidden="true"></i>Log in with
           Spotify
         </button>
-        <AliceCarousel autoPlay autoPlayInterval="3000">
-          <img src={image1} className="sliderimg"/>
-          <img src={image2} className="sliderimg"/>
-          <img src={image3} className="sliderimg"/>
-          <img src={image4} className="sliderimg"/>
-          <img src={image5} className="sliderimg"/>
-          <img src={image6} className="sliderimg"/>
-        </AliceCarousel>
+        <Carousel />
       </div>
     </div>
   );
