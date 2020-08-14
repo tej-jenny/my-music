1dbb02ec6b3352a358eb1c02ccb6ad3cdbc9ae69
Jenny Dinh <jennythi.d@gmail.com>
2020-08-13 21:50:49 -0400

    move components around

--- src/Components/MainProject/Main.js
+++ src/Components/MainProject/Main.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./Home";
 import Browse from "./Browse";
+import MainController from "./../Controller/MainController";
+import SideNav from "./../SideNav/SideNav";
 
 const Main = ({ logOut }) => {
+
+  useEffect(() => {   
+    console.log("Hello"); 
+  });
+  
   return (
     <div>
       <button className="btn logout" onClick={() => logOut()}>
@@ -10,6 +17,8 @@ const Main = ({ logOut }) => {
       </button>
       <Home />
       <Browse />
+      <SideNav />
+      <MainController />
     </div>
   );
 };
